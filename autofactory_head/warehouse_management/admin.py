4585bbe5aa594f7047960ca946fa3f99b6e5b693
kirflaer <kirflaer@gmail.com>
2023-02-08 10:08:20 +0300

    1.2.3.3 Доработка админки для состояния складских ячеек

--- autofactory_head/warehouse_management/admin.py
+++ autofactory_head/warehouse_management/admin.py
@@ -167,6 +167,7 @@ class StorageAreaAdmin(admin.ModelAdmin):
 class StorageCellContentStateAdmin(admin.ModelAdmin):
     list_display = ('creating_date', 'cell', 'pallet', 'status')
     search_fields = ('cell__name', 'pallet__id', 'pallet__guid')
+    list_filter = (('creating_date', DateRangeFilter),)
 
 
 @admin.register(RepackingOperation)

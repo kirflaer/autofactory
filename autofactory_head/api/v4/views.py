c52bfc9e6d524c6449bfc8a6c5b4e54389b291b8
kirflaer <kirflaer@gmail.com>
2023-03-15 14:46:22 +0300

    1.2.5.1 Исправлена ошибка с отсутствием задания

--- autofactory_head/api/v4/views.py
+++ autofactory_head/api/v4/views.py
@@ -1,9 +1,9 @@
-from api.v2.views import TasksChangeViewSet
+from api.v3.views import TasksViewSet
 from api.v4.routers import get_task_router
 from tasks.task_services import RouterTask
 
 
-class TasksViewSet(TasksChangeViewSet):
+class TasksViewSet(TasksViewSet):
     def get_routers(self) -> dict[str: RouterTask]:
         parent_routers = super().get_routers()
         return parent_routers | get_task_router()

ec03b6d433fbeaf5558c0fc96ae7a2c1422d6acc
kirflaer <kirflaer@gmail.com>
2023-05-30 14:57:50 +0300

    1.2.7.2 Доработка пакета создания адресной инвентаризации

--- autofactory_head/warehouse_management/serializers.py
+++ autofactory_head/warehouse_management/serializers.py
@@ -686,7 +686,7 @@ class OperationPalletSerializer(serializers.Serializer):
 class InventoryAddressWarehouseSerializer(serializers.ModelSerializer):
     product = serializers.CharField()
     pallet = PalletReadSerializer()
-    cell = serializers.SlugRelatedField(slug_field='external_key', read_only=True)
+    cell = serializers.SlugRelatedField(slug_field='guid', read_only=True)
     plan = serializers.IntegerField()
     fact = serializers.IntegerField(required=False)
 

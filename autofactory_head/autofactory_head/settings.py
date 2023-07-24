147778e308d66a6c316054ccb8c3a9dd543d2e6c
kirflaer <kirflaer@gmail.com>
2023-07-24 10:50:20 +0300

    1.5.5.0 Смена версии

--- autofactory_head/autofactory_head/settings.py
+++ autofactory_head/autofactory_head/settings.py
@@ -117,5 +117,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 LOGIN_URL = "/auth/login/"
 LOGIN_REDIRECT_URL = "index"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-VERSION = '1.5.4.0'
+VERSION = '1.5.5.0'
 

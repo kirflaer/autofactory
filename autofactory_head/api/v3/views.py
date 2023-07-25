62df7817d7dd1848bc757a260e55c01dfe9e5a69
kirflaer <kirflaer@gmail.com>
2023-07-25 09:14:46 +0300

    1.5.5.0 Задержка 20 секунд

--- autofactory_head/api/v3/views.py
+++ autofactory_head/api/v3/views.py
@@ -155,7 +155,6 @@ class PalletShipmentUpdate(generics.UpdateAPIView):
                 case 'done':
                     return Response(serializer.data)
                 case 'wait':
-                    time.sleep(25)
                     return Response(status=102)
                 case _:
                     cache_data['status'] = 'wait'
